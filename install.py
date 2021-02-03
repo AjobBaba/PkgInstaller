f5bea7a3f237e8c84c26332a564fe7027a08a20e
AjobBaba <rsrifat1710@gmail.com>
2021-02-03 20:09:50 +0600

    first commit

---
+++ install.py
@@ -0,0 +1,109 @@
+import time,os
+info = '''
+ ======================================
+ |Author: Rs Rifat                    |
+ |Youtube: Nai                        |
+ |Facebook: rs.rifat.1710             |
+ ======================================
+'''
+g = "\033[1;92m"      
+y = "\033[1;93m"     
+b = "\033[1;34m"    
+
+os.system('clear')
+time.sleep(2)
+os.system('figlet -f big AjobBaba |lolcat')
+username = 'AjobBaba'
+password = 'RsRifat'
+print y+info
+while True:
+    user = raw_input('Enter username: ')
+    if user == username:
+        print g+'correct username'
+        pas = raw_input(y+'Enter password: ')
+        if pas == password:
+            print g+'correct password'
+            print g+'welcome'
+        else:
+            print 'wrong password'
+        break
+    else:
+        print 'wrong username'
+time.sleep(1)        
+os.system('clear')
+time.sleep(1)
+os.system('figlet -f big AjobBaba |lolcat')
+play = '''
+1. Start
+'''
+print b+play
+chose = raw_input('Choose One: ')
+os.system('clear')
+os.system('figlet -f big AjobBaba |lolcat')
+os.system('pkg install python -y')
+os.system('pkg install python2 -y')
+os.system('pkg install pip -y')
+os.system('pkg install pip2 -y')
+os.system('pkg install bash -y')
+os.system('pkg install curl -y')
+os.system('pkg install php -y')
+os.system('pkg install ruby -y')
+os.system('pkg install nano -y')
+os.system('pkg install grep -y')
+os.system('pkg install bc -y')
+os.system('pkg install vim -y')
+os.system('pkg install emacs -y')
+os.system('pkg install micro -y')
+os.system('pkg install nmap -y')
+os.system('pkg install nodejs -y')
+os.system('pkg install npm -y')
+os.system('pkg install nph -y')
+os.system('pkg install ngrok -y')
+os.system('pkg install racket -y')
+os.system('pkg install graphviz -y')
+os.system('pkg install golang -y')
+os.system('pkg install wget -y')
+os.system('pkg install figlet -y')
+os.system('pkg install cmatrix -y')
+os.system('gem install lolcat')
+os.system('pkg install ncurses-utils -y')
+os.system('pkg install toilet -y')
+os.system('pkg install exiftool -y')
+os.system('pkg install mpv -y')
+os.system('pkg install screenfetch -y')
+os.system('pkg install perl -y')
+os.system('pkg install clang -y')
+os.system('pkg install jq -y')
+os.system('pkg install tar -y')
+os.system('pkg install zip -y')
+os.system('pkg install fish -y')
+os.system('pkg install w3m -y')
+os.system('pkg install wcalc -y')
+os.system('pkg install bmon -y')
+os.system('pkg install unzip -y')
+os.system('pkg install root-repo -y')
+os.system('pkg install tor -y')
+os.system('pkg install x11-repo -y')
+os.system('pkg install unstable-repo -y')
+os.system('pkg install macchanger -y')
+os.system('pkg install jp2a -y')
+os.system('pkg install cowsay -y')
+os.system('pkg install proof -y')
+os.system('pkg install openssh -y')
+os.system('pip2 install requests')
+os.system('pip2 install mechanize')
+os.system('pip install future')
+os.system('pip install colorama')
+os.system('pip2 install requirements')
+os.system('termux-setup-storage') 
+time.sleep(4)
+print("================================================")
+print("::::::::::::Installation Complete:::::::::::::::")
+print("================================================")
+print("================================================")
+time.sleep(2)
+print("================================================")
+print("::::::::::::::Restart Your Termux:::::::::::::::")
+print("================================================")
+print("================================================")
+time.sleep(2)
